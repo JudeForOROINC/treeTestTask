5ca42edf03b7d8945eec4a8cc1fbd32d1d70fd7d
JudeForOROINC <gkudryashov@magecore.com>
2015-08-08 19:47:20 +0400

    structure generate

---
+++ src/Sparkle/Bundle/TreeBundle/Entity/Position.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Sparkle\Bundle\TreeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Position
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Sparkle\Bundle\TreeBundle\Entity\PositionRepository")
+ */
+class Position
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="type_id", type="integer")
+     */
+    private $typeId;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="category_id", type="integer")
+     */
+    private $categoryId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="position_name", type="string", length=255)
+     */
+    private $positionName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set typeId
+     *
+     * @param integer $typeId
+     * @return Position
+     */
+    public function setTypeId($typeId)
+    {
+        $this->typeId = $typeId;
+
+        return $this;
+    }
+
+    /**
+     * Get typeId
+     *
+     * @return integer 
+     */
+    public function getTypeId()
+    {
+        return $this->typeId;
+    }
+
+    /**
+     * Set categoryId
+     *
+     * @param integer $categoryId
+     * @return Position
+     */
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryId
+     *
+     * @return integer 
+     */
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    /**
+     * Set positionName
+     *
+     * @param string $positionName
+     * @return Position
+     */
+    public function setPositionName($positionName)
+    {
+        $this->positionName = $positionName;
+
+        return $this;
+    }
+
+    /**
+     * Get positionName
+     *
+     * @return string 
+     */
+    public function getPositionName()
+    {
+        return $this->positionName;
+    }
+}
